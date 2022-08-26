1c4e7b96a57f1872db01ccbd210e1aa7dba65d0b
MDBossss <ferrari020203@gmail.com>
2022-08-26 21:11:01 +0200

    coingecko api

---
+++ src/components/CoinLabel/CoinLabel.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./coinLabel.js"
+
+const CoinLabel = ({coin}) => {
+  return (
+    <div>CoinLabel</div>
+  )
+}
+
+export default CoinLabel
--- src/pages/charts/Charts.js
+++ src/pages/charts/Charts.js
@@ -1,20 +1,26 @@
 import React from 'react'
-import { useEffect } from 'react';
+import { useState,useEffect } from 'react';
 import axios from "axios"
 import "./charts.css";
 
 const Charts = () => {
 
+  const [data,setData] = useState([])
+
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(result => {
-      console.log(result)
+      setData(result.data)
     })
-  })
+  },[])
+
+  console.log(data)
 
 
   return (
-    <div>Charts</div>
+    <>
+      <h2>Crypto Prices</h2>
+    </>
   )
 }
 
