e648be60f78bb836b266b0975039f42802e1783c
MDBossss <ferrari020203@gmail.com>
2022-08-27 21:32:14 +0200

    updated

--- src/components/CoinLabel/CoinLabel.js
+++ src/components/CoinLabel/CoinLabel.js
@@ -1,9 +1,29 @@
 import React from 'react'
-import "./coinLabel.js"
+import {useState} from "react"
+import "./coinLabel.css"
+import PriceChange from '../PriceChange/PriceChange'
 
 const CoinLabel = ({coin}) => {
-  return (
-    <div>CoinLabel</div>
+
+    const [expandLabel,setExpandLabel] = useState(false)
+
+ return (
+    <div className="coin-label" onClick={() => setExpandLabel(!expandLabel)}>
+        <div className="main-info">
+            <div className="name">
+                <span>{coin.market_cap_rank}</span>    
+                <div><img src={coin.image} alt="coin" /></div>
+                <span>{coin.name}</span>
+            </div>
+            <div className="prices">
+                <PriceChange priceChange={coin.price_change_percentage_24h}/>
+                <span>${coin.current_price}</span>
+            </div>
+        </div>
+        {expandLabel && <div className="extended-info">
+            <h2>hello</h2>
+            </div>}
+    </div>
   )
 }
 
---
+++ src/components/PriceChange/PriceChange.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {IoMdArrowDropdown, IoMdArrowDropup} from "react-icons/io"
+import "./priceChange.css"
+
+const PriceChange = ({priceChange}) => {
+
+  return (
+    <>
+        {priceChange > 0 ? <div className='change positive'>
+            <IoMdArrowDropup/>
+            <span>{priceChange.toFixed(2)}%</span>
+        </div> : 
+        <div className='change negative'>
+            <IoMdArrowDropdown/>
+            <span>{priceChange.toFixed(2)}%</span>
+        </div>}
+    </>
+  )
+}
+
+export default PriceChange
