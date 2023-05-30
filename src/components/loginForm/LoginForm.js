a5da34b21f7d6fdc742aab783436307aed40c37c
MDBossss <ferrari020203@gmail.com>
2023-05-30 22:21:38 +0200

    updates

--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -1,51 +1,52 @@
-import React from 'react'
-import {useState,useContext} from "react";
-import Button from '../button/Button';
-import {signInWithEmailAndPassword } from "firebase/auth";
-import {auth} from "../../firebase";
-import {useNavigate} from "react-router-dom";
-import { AuthContext } from '../../context/AuthContext';
-import "./loginForm.scss"
+import React from "react";
+import { useState, useContext } from "react";
+import Button from "../button/Button";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import "./loginForm.scss";
 
-const LoginForm = ({loginState,setLoginState}) => {
+const LoginForm = ({ loginState, setLoginState }) => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState(false);
 
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
-  const [error,setError] = useState(false);
+	const navigate = useNavigate();
 
-  const navigate = useNavigate()
+	const { dispatch } = useContext(AuthContext);
 
-  const {dispatch} = useContext(AuthContext)
+	const handleLogin = () => {
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in
+				const user = userCredential.user;
 
-  const handleLogin = () => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
+				console.log("signed in");
+				setError(false);
+				dispatch({ type: "LOGIN", payload: user });
+				navigate("/");
+			})
+			.catch((error) => {
+				setError(true);
+				console.log(error);
+			});
+	};
 
-        console.log("signed in")
-        setError(false);
-        dispatch({type:"LOGIN",payload:user})
-        navigate("/");
-      })
-      .catch((error) => {
-        setError(true);
-        console.log(error)
-      });
-  }
+	return (
+		<div className="form">
+			{error && <span className="alert">Wrong password or email!</span>}
+			<label>E-mail</label>
+			<input type="email" onChange={(e) => setEmail(e.target.value)} />
+			<label>Password</label>
+			<input type="password" onChange={(e) => setPassword(e.target.value)} />
+      <span>test login info: <br/> test@gmail.com | test12</span>
+			<div className="empty" onClick={handleLogin}>
+				<Button text="Login" />
+			</div>
+			<span onClick={() => setLoginState(!loginState)}>Register</span>
+		</div>
+	);
+};
 
-
-  return (
-    <div className='form'>
-      {error && <span className='alert'>Wrong password or email!</span>}
-      <label>E-mail</label>
-      <input type="email" onChange={(e) => setEmail(e.target.value)}/>
-      <label>Password</label>
-      <input type="password" onChange={(e) => setPassword(e.target.value)}/>
-      <div className='empty' onClick={handleLogin}><Button text="Login"/></div>
-      <span onClick={() => setLoginState(!loginState)}>Register</span>
-    </div>
-  )
-}
-
-export default LoginForm
+export default LoginForm;
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,44 +1,58 @@
-import React from 'react'
-import { useEffect } from 'react';
-import {useState} from "react";
-import Post from '../post/Post';
-import Loading from '../loading/Loading';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import Post from "../post/Post";
+import Loading from "../loading/Loading";
+import axios from "axios";
+
+const Posts = ({ keyword }) => {
+	const [articles, setArticles] = useState([]);
+	const [status, setStatus] = useState("failed");
+	const [starVisible, setStarVisible] = useState(true);
 
 
+	const options = {
+		method: "GET",
+		url: "https://api.newscatcherapi.com/v2/search",
+		params: { q: keyword, lang: "en", sort_by: "relevancy", page: "1" },
+		headers: {
+			"x-api-key": process.env.REACT_APP_NEWS_API,
+		},
+	};
 
-const Posts = ({keyword}) => {
+	useEffect(() => {
+		axios
+			.request(options)
+			.then((response) => {
+				setArticles(response.data.articles);
+				setStatus(response.data.status);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, [keyword]);
 
-    const [articles,setArticles] = useState([]);
-    const [status,setStatus] = useState("failed");
-    const [starVisible,setStarVisible] = useState(true)
+	return (
+		<>
+			{status === "ok" ? (
+				articles?.map((article, index) => (
+					<Post
+						key={index}
+						starVisible={starVisible}
+						article={article}
+						author={article.author}
+						description={article.summary}
+						sourceName={article.clean_url}
+						title={article.title}
+						url={article.link}
+						imageUrl={article.media}
+					/>
+				))
+			) : (
+				<Loading />
+			)}
+		</>
+	);
+};
 
-    var axios = require("axios").default;
-    var options = {
-        method: 'GET',
-        url: 'https://api.newscatcherapi.com/v2/search',
-        params: {q: keyword, lang: 'en', sort_by: 'relevancy', page: '1'},
-        headers: {
-          'x-api-key': 'lS7wl5m8wb7-veU1o6EFcsWMmMdrjLU5BfB-m3xD1gI'
-        }
-      };
-
-    useEffect(() => {
-        axios.request(options).then(function (response) {
-            setArticles(response.data.articles);
-            setStatus(response.data.status);
-        }).catch(function (error) {
-            console.error(error);
-        });
-    },[keyword])
-
-  return (
-    <>  
-        {status === "ok" ? articles?.map((article,index) => (
-            <Post key={index} starVisible={starVisible} article={article} author={article.author} description={article.summary}  sourceName={article.clean_url} title={article.title} url={article.link} imageUrl={article.media}/>
-        )) : <Loading/>}
-    </>
-    
-  )
-}
-
-export default Posts
+export default Posts;
